886f021f5a1a25c30ef0f4407115995ea8a9fcac
weiaiwayne <weiai.wayne.xu@gmail.com>
2020-11-20 18:54:13 -0500

    Auto Server Commit 20.11.2020 um 18:54

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 cd /home/curiositybits/icacmtracker/icacmtracker-public
-
 read message
 git add *
-
 timestamp(){
    date +"%d.%m.%Y um %H:%M"
 }
 git commit -am "Auto Server Commit $(timestamp)"
-
 git push
